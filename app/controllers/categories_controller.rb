770bd1524a80338dddff348493ce1ab4cd30b92d
Kominam <kominam.2511@gmail.com>
2016-12-13 16:35:34 +0700

    gem setting config

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,6 +2,6 @@ class CategoriesController < ApplicationController
 
   def index
     @categories = Category.search(params[:search]).includes(:words).recent
-      .paginate page: params[:page]
+      .paginate page: params[:page], per_page: Settings.per_page
   end
 end
